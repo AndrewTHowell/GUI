fbab52edb2cc8fe9b881ce29e50b9901e70d8f71
AndrewTHowell <howelldrew99@gmail.com>
2020-06-11 12:24:40 +0100

    QLabel created and aligned

--- GUI.py
+++ GUI.py
@@ -1,10 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("My First GUI App")
+
+        label = QLabel("This is a PyQt5 window")
+
+        label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(label)
+
 
 # Allow CLI arguments - use 'app = QApplication([])' if no CLI functionality wanted
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+window = MainWindow()
 window.show()  # Windows are hidden by default, so need to show it
 
 # PyQt5 event loop
