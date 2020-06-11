e4cae04bc114b3b91deec1282a403ea9b7454833
AndrewTHowell <howelldrew99@gmail.com>
2020-06-11 12:20:06 +0100

    PyQt5 QMainWindow created - advantageous over QWidget

--- GUI.py
+++ GUI.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 # Allow CLI arguments - use 'app = QApplication([])' if no CLI functionality wanted
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = QMainWindow()
 window.show()  # Windows are hidden by default, so need to show it
 
 # PyQt5 event loop
