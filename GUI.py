8572c24af06e0b56bdd7423221d968bc3bce39c1
AndrewTHowell <howelldrew99@gmail.com>
2020-06-11 12:17:39 +0100

    PyQt5 Application created

--- GUI.py
+++ GUI.py
@@ -1,1 +1,8 @@
-import pyqt5
+import sys
+from PyQt5.QtWidgets import QApplication
+
+# Allow CLI arguments - use 'app = QApplication([])' if no CLI functionality wanted
+app = QApplication(sys.argv)
+
+# PyQt5 event loop
+app.exec_()
