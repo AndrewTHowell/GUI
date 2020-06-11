0571cf96e3cf59d2e1bac48e0f3a985059b2f88c
AndrewTHowell <howelldrew99@gmail.com>
2020-06-11 12:19:10 +0100

    PyQt5 QWidget Window created

--- GUI.py
+++ GUI.py
@@ -1,8 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget
 
 # Allow CLI arguments - use 'app = QApplication([])' if no CLI functionality wanted
 app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()  # Windows are hidden by default, so need to show it
 
 # PyQt5 event loop
 app.exec_()
